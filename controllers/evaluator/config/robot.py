f9e925b3df2b796c4c93cae9c961dab4a5c78b99
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-23 17:11:50 +0100

    added configuration file to specify robot used devices

---
+++ controllers/evaluator/config/robot.py
@@ -0,0 +1,20 @@
+""" CONFIGURATION OF ROBOT MODULES """
+
+from controllers.evaluator.components import sensors, motors
+
+used_sensors = [
+    # front
+    sensors['A335'],
+    sensors['A25'],
+
+    # side
+    sensors['A45'],
+    sensors['A90'],
+    sensors['A315'],
+    sensors['A270']
+]
+
+used_actuators = [
+    motors['LEFT'],
+    motors['RIGHT']
+]
