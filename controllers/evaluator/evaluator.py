9de37709e334cb5460eca9046e7f846d5c131005
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-25 16:25:08 +0100

    cleaned and reorganized code; corrected bug that didn't allow mutation of input nodes

--- controllers/evaluator/evaluator.py
+++ controllers/evaluator/evaluator.py
@@ -5,22 +5,16 @@ from config.simulation import *
 from config.network import *
 from epuck import EPuck
 
+################################################################################
+# ROBOT SETUP
+
 # crate robot controller
 conductor = Conductor(graph, datasheet)
 
 # create the Robot instance
 robot = EPuck(conductor)
 
-# get measure function
-# https://cyberbotics.com/doc/guide/supervisor-programming?tab-language=python
-node = robot.getFromDef('evolvable')
-trans_field = node.getField('translation')
-
-# reset the simulation
-robot.simulationReset()
-robot.simulationSetMode(robot.SIMULATION_MODE_FAST)
-
-# randomly connect the actuators to the network
+# randomly connect the actuators(motors) to the network
 output_nodes = random_nodes(conductor.network, avoid=set(), count=2)
 mapping = dict(zip(robot.motors, output_nodes))
 conductor.set_actuators(robot.motors, mapping)
@@ -31,17 +25,35 @@ viable_nodes = [*source_selector(conductor.network, [], -1)]
 
 # randomly select input nodes between the ones that are suitable for the
 # selection
-input_nodes = map(
-    lambda _: viable_nodes[random.randrange(len(viable_nodes))], robot.sensors
-)
+# the selected nodes may be repeated
+input_nodes = [
+    viable_nodes[random.randrange(len(viable_nodes))] for _ in robot.sensors
+]
 mapping = dict(zip(robot.sensors, input_nodes))
 conductor.set_sensors(robot.sensors, mapping)
 
+# initialize the network with the given setting
 conductor.initialize()
 
-# simulate count epochs run
-for _ in range(epoch_count):
+################################################################################
+# DEFINE SIMULATIONS UTILITIES
 
+# get measure function
+# https://cyberbotics.com/doc/guide/supervisor-programming?tab-language=python
+node = robot.getFromDef('evolvable')
+trans_field = node.getField('translation')
+
+# reset the simulation
+robot.simulationReset()
+robot.simulationSetMode(robot.SIMULATION_MODE_FAST)
+
+################################################################################
+# RUN THE SIMULATION
+
+# simulate count epochs run
+for epoch in range(epoch_count):
+
+    print(f'Start of {epoch} simulation')
     # perform simulation steps until a epoch is completed
     for _ in range(epoch_duration):
 
@@ -50,14 +62,15 @@ for _ in range(epoch_count):
 
     # randomly connect the sensors to the network
     input_nodes = mutate(
-        conductor.network,
-        input_nodes,
+        graph=conductor.network,
+        sources=input_nodes,
         ground=-1,
         probability=0.3,
         minimum_mutants=1,
         maximum_mutants=4,
         viable_node_selection=source_selector
     )
+
     mapping = dict(zip(robot.sensors, input_nodes))
     conductor.set_sensors(robot.sensors, mapping)
 
