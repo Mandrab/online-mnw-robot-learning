4a8e390459cb36f525e6d58e334f5de9fc3b950d
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-29 15:36:26 +0100

    added analysis of the load influence in the network behaviour

--- analysis.py
+++ analysis.py
@@ -36,8 +36,9 @@ c.sensors = dict(zip(['s'], sensors))
 
 c.initialize()
 
+
 ################################################################################
-# RESPONSE ANALYSIS
+# RESPONSE ANALYSIS IN FREQUENCY
 # The following graphs shows the variance of the motor output, in a random
 # connection, according to the sensor input at different frequencies.
 # When the frequency is high, the best path is continuously/highly stimulated
@@ -49,7 +50,7 @@ c.initialize()
 # their behaviour runtime, possibly increasing capabilities.
 
 def __influence(values):
-    fig, axs = plt.subplots(len(values) + 1, 1, figsize=(8, 20))
+    fig, axs = plt.subplots(len(values) + 1, 1, figsize=(7, 16))
     values_ax, *axs = axs
 
     for ax, value in zip(axs, values):
@@ -89,6 +90,83 @@ def __influence(values):
     values_ax.set(xlabel='Sensors signals', ylabel='Motor outputs')
     values_ax.legend()
 
+    plt.subplots_adjust(top=0.95, bottom=0.03, hspace=0.4)
+
+    plt.suptitle(
+        'Analysis of the update frequency influence in the signal propagation'
+    )
     plt.show()
 
+
 __influence([0.1, 5, 10])
+
+
+################################################################################
+# RESPONSE ANALYSIS IN FREQUENCY
+# The following graphs show the influence of the motor resistance in the
+# stimulation of the network.
+# When the resistance is low, just a small amount of tension 'fall' at the sides
+# of the motor. This cause the network to be highly stimulated, in that the vast
+# majority of the tension falls inside the device.
+# todo Sure? Check paper. Make graph of voltage fall (V_source - V_output)
+# Differently, when the device is connected to an 'heavy' load, an high voltage
+# is measurable at the sides of the motor. This makes the network just softly
+# stimulated, and prevents any interesting plasticity to show-up.
+# It is also interesting to note that an high load directly influences the motor
+# speed. Indeed, an higher voltage will insists on the motor, making it move
+# fastly.
+# The said properties are opposite: high plasticity brings to low speed; low
+# plasticity to high speed. A complex system may need both. A solution may be to
+# adjust the output value to the correct range, however, this would complicate
+# the solution. It is then clear that the search of a balance is needed.
+
+def __influence(values):
+    fig, axs = plt.subplots(len(values) + 1, 1, figsize=(7, 16))
+    values_ax, *axs = axs
+
+    for ax, value in zip(axs, values):
+        # avoid modify the graph
+        c.network = graph.copy()
+
+        step = lambda i: c.evaluate(
+            update_time=0.1,  # seconds
+            inputs={'s': i},
+            inputs_range=sensor_range,
+            outputs_range=motors_range,
+            actuators_load=value
+        )
+
+        io = {
+            i: [v for v in step(i).values()][0]
+            for i in range(*sensor_range, 128)
+        }
+
+        # make data
+        x = [*io.keys()]
+        y = [*io.values()]
+
+        values_ax.plot(x, y, label=f'resistance {1.0 / value}')
+
+        e = Evolution(
+            datasheet,
+            wires_dict={}, delta_time=value, grounds=set(),
+            loads={(a, 1) for a in actuators},
+            network_instances=[
+                (c.network, [(s, sensor_range[1]) for s in sensors])
+            ])
+
+        plot.conductance_map(fig, ax, e)
+        ax.set_title(f'Conductance in {1 / value}Hz updated network')
+
+    values_ax.set(xlabel='Sensors signals', ylabel='Motor outputs')
+    values_ax.legend()
+
+    plt.subplots_adjust(top=0.95, bottom=0.03, hspace=0.4)
+
+    plt.suptitle(
+        'Analysis of the motor load influence in the signal propagation'
+    )
+    plt.show()
+
+
+__influence([1, 100, 10000])
