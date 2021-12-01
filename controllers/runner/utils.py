3c67547cdb9f7a5090d87113a9f9ef7ca06e6637
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-01 16:34:01 +0100

    corrected bug in adapt that prevented to use reverse range (e.g. [10, -10])

--- controllers/runner/utils.py
+++ controllers/runner/utils.py
@@ -1,6 +1,7 @@
 import operator
 
 from functools import reduce
+from math import copysign
 from typing import Tuple
 
 
@@ -11,13 +12,14 @@ def adapt(
 ) -> float:
     """Adapt a value to a different range"""
 
-    in_delta = reduce(operator.__sub__, reversed(in_range))
-    out_delta = reduce(operator.__sub__, reversed(out_range))
-    value = out_range[0] + (value - in_range[0]) * out_delta / in_delta
+    in_delta: float = reduce(operator.__sub__, reversed(in_range))
+    out_delta: float = reduce(operator.__sub__, reversed(out_range))
+    value = (value - min(in_range)) * out_delta / in_delta
+    value = out_range[0] + copysign(value, out_delta)
 
     # force bounds to the value
-    value = min(value, out_range[1])
-    value = max(value, out_range[0])
+    value = min(value, max(out_range))
+    value = max(value, min(out_range))
 
     return value
 
