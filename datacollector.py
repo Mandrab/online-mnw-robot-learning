c09829ce983ee577da4ab68bb9cc1446fe668341
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-03 13:24:03 +0100

    created robot to get movement data to be used for network behaviour analysis

---
+++ datacollector.py
@@ -0,0 +1,14 @@
+import subprocess
+
+__WORLD_FILE = 'worlds/datacollector_world.wbt'
+
+################################################################################
+# START OF SIMULATION
+
+# start a simulation subprocess
+subprocess.run([
+    'webots ' +
+    '--batch ' +        # does not create blocking pop-ups
+    '--log-performance=stdout ' +   # measure the performance
+    __WORLD_FILE
+], shell=True, check=True)
