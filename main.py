e689b97e6a7daae4bebc70b46a0bdf0911732b98
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-06 14:12:57 +0100

    moved world file and changes webots launch

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import subprocess
 
-__WORLD_FILE = 'worlds/world.wbt'
+__WORLD_FILE = 'worlds/main_world.wbt'
 
 ################################################################################
 # START OF SIMULATION
 
 # start a simulation subprocess
 subprocess.run([
-    'webots',
-    '--mode=fast',      # fast running
-    '--no-rendering',   # disable rendering
-    '--minimize',       # minimize the window on startup
-    '--batch',          # does not create blocking pop-ups
-    '--stdout',         # redirect robot out to stdout
-    '--stderr',         # redirect robot errors to stderr
-    '--log-performance=stdout',     # measure the performance
+    'webots ' +
+    '--mode=fast ' +        # fast running
+    '--no-rendering ' +     # disable rendering
+    '--minimize ' +         # minimize the window on startup
+    '--batch ' +            # does not create blocking pop-ups
+    '--stdout ' +           # redirect robot out to stdout
+    '--stderr ' +           # redirect robot errors to stderr
+    '--log-performance=stdout ' +   # measure the performance
     __WORLD_FILE
 ], shell=True, check=True)
