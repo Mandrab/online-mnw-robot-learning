bbbfd69784c379c3c72c9689bbe4743a9d2dbc42
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-07 12:23:21 +0100

    added save of best controllers for analysis; cleaned runner code and added comments

--- controllers/runner/runner.py
+++ controllers/runner/runner.py
@@ -1,46 +1,66 @@
+import time
+
+from conductor import Conductor
 from config.simulation import *
 from optimization.Simulation import Simulation
 from nanowire_network_simulator.model.device import Datasheet
+from nanowire_network_simulator import backup
 from typing import List
 
+DEVICE_SIZE = 50
+WIRES_LENGTH = 10.0
+
+# create a list of simulations to fill with each density simulation
 simulations: List[Simulation] = []
 
+# for each density, create and test a device as a controller
 for density in densities:
 
-    size = 50
-    length = 10.0
-    wires = int(density * size * size / (length * length))
-
+    # calculate number of needed wires
+    wires = int(density * DEVICE_SIZE ** 2 / WIRES_LENGTH ** 2)
     print('Network density:', density, 'wires:', wires)
 
     # define network characteristics
     datasheet = Datasheet(
         wires_count=wires,
-        Lx=size,
-        Ly=size,
-        mean_length=length,
-        std_length=length * 0.35
+        Lx=DEVICE_SIZE, Ly=DEVICE_SIZE,
+        mean_length=WIRES_LENGTH, std_length=WIRES_LENGTH * 0.35
     )
 
+    # set simulation to fast mode
     robot.simulationSetMode(robot.SIMULATION_MODE_FAST)
 
+    # instantiate simulation with the given controller/device
     simulation = Simulation(robot, datasheet)
 
-    # simulate count epochs run
+    # simulate count-epochs run
     for epoch in range(epoch_count):
         simulation.simulate(epoch_duration)
 
+    # save simulation results
     simulations += [simulation]
 
 print("Running the best scoring controller")
-
 robot.simulationReset()
-robot.simulationSetMode(robot.SIMULATION_MODE_FAST)
 
-scores = [(s.best_epoch.fitness.value(), s.best_epoch) for s in simulations]
-_, best = next(iter(sorted(scores, key=lambda e: -e[0])))
+# get the best configurations found for each controller
+bests = list(map(lambda s: s.best_epoch, simulations))
 
+# save the configurations in a file
+for index, epoch in enumerate(bests):
+    c: Conductor = epoch.controller
+    ms_time = time.time()
+    backup.save(
+        c.datasheet, c.network, c.wires,
+        f'datasheet.{ms_time}.{index}.dat',
+        f'datasheet.{ms_time}.{index}.dat'
+        f'datasheet.{ms_time}.{index}.dat'
+    )
+
+# get the best configuration overall
+best = max(bests, key=lambda e: e.fitness.value())
+
+# run the best controller until it is stopped
 robot.conductor = best.controller
-
-while True:
-    robot.run()
+while robot.run():
+    pass
