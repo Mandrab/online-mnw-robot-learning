c36b48281e5764451af1f277e0189ec6e100fb68
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-07 12:30:49 +0100

    varing only density in simulation: not all the parameters

--- controllers/runner/config/simulation.py
+++ controllers/runner/config/simulation.py
@@ -10,18 +10,10 @@ random.seed(1234)
 epoch_count = 20
 
 # number of steps for which the robot can run freely
-epoch_duration = 150
+epoch_duration = 300
 
-# network configurations to tests
-# syntax: (wires count, device size, wire length)
-grid = [
-    (100, 45, 10),      # density 4.94
-    (100, 40, 9),       # density 5.06
-    (100, 35, 8),       # density 5.22
-    (250, 40, 6),       # density 5.63
-    (400, 50, 6),       # density 5.76
-    (400, 80, 10)       # density 6.25
-]
+# network (density) configurations to tests
+densities = [_ / 10.0 for _ in range(40, 251, 23)]
 
 # create the Robot instance
 robot = EPuck()
