3238e4f2acee7c6f0a89ad091d1aa0bc59519ead
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-18 15:25:04 +0100

    improved start-headless script

--- scripts/start_headless.sh
+++ scripts/start_headless.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-# Create a fake desktop and open webots in it.
-# It is possible to set up webots to run automatically,
-# without human activation.
+# Create a fake desktop, setup a virtual environment and open webots simulation
+# in it.
+
+################################################################################
+# DISPLAY HELP
+
+if [[ $1 == '-h' ]]; then
+  echo '
+    Create a fake desktop, setup a virtual environment and open webots
+    simulation in it.
+
+    Synopsys:
+      /path/to/start_headless.sh -h | MAIN_FILE.py
+  '
+  exit
+fi
+
+################################################################################
+# DESKTOP SETUP
 
 # define desktop environmental variables
 export DEBIAN_FRONTEND=noninteractive
@@ -11,5 +27,20 @@ export LIBGL_ALWAYS_SOFTWARE=true
 # start a virtual screen with Xvfb
 Xvfb :99 -screen 0 1024x768x16 &
 
-# start webots
-webots
+################################################################################
+# VIRTUAL ENVIRONMENT SETUP
+
+# start virtual environment (and create if it does not exists)
+if [[ ! -d venv ]]; then
+  python3 -m venv venv
+fi
+source venv/bin/activate
+
+# install pip requirements
+pip install -r requirements.txt
+
+################################################################################
+# SIMULATION START
+
+# start webots simulation from .py specified file
+python $1
