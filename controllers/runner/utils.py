092791bf2aa7b5c98198956c41cea24e50b1882d
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-21 14:14:44 +0100

    changes to improve code cleaning

--- controllers/runner/utils.py
+++ controllers/runner/utils.py
@@ -1,5 +1,6 @@
 import operator
 
+from dataclasses import dataclass
 from functools import reduce
 from math import copysign
 from typing import Tuple
@@ -7,30 +8,34 @@ from typing import Tuple
 
 def adapt(
         value: float,
-        in_range: Tuple[float, float],
-        out_range: Tuple[float, float]
+        in_range: Tuple[float, float] = (0.0, 1.0),
+        out_range: Tuple[float, float] = (0.0, 1.0)
 ) -> float:
-    """Adapt a value to a different range"""
+    """Adapt a value in a range to a different one."""
 
     in_delta: float = reduce(operator.__sub__, reversed(in_range))
     out_delta: float = reduce(operator.__sub__, reversed(out_range))
     value = (value - min(in_range)) * out_delta / in_delta
     value = out_range[0] + copysign(value, out_delta)
 
-    # force bounds to the value
-    value = min(value, max(out_range))
-    value = max(value, min(out_range))
-
-    return value
+    # force bounds to the value and return it
+    return max(min(value, max(out_range)), min(out_range))
 
 
+@dataclass(frozen=True)
 class Frequency:
     """
     Represents the working frequency of the robot.
     Being a class, it avoids usage of different measure units for error.
     """
 
-    def __init__(self, hz_value: float):
-        self.Hz: float = hz_value
-        self.s: float = 1.0 / self.Hz
-        self.ms: int = int(self.s * 1000.0)
+    hz_value: float
+
+    @property
+    def Hz(self) -> float: return self.hz_value
+
+    @property
+    def s(self) -> float: return 1.0 / self.Hz
+
+    @property
+    def ms(self) -> int: return int(self.s * 1000.0)
