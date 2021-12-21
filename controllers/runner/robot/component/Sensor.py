d3bd54050a3d46d384b13e02844004c03c09cb6e
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-21 14:27:53 +0100

    make upper class to represent sensor

---
+++ controllers/runner/robot/component/Sensor.py
@@ -0,0 +1,32 @@
+from abc import abstractmethod
+from controller import Robot
+from typing import Tuple
+from utils import adapt
+
+
+class Sensor(str):
+    """
+    Represents a sensor of the robot and act as a proxy with more user friendly
+    methods.
+    """
+
+    # the robot that it is working for/in
+    robot: Robot
+
+    @abstractmethod
+    def range(self, **_) -> Tuple[float, float]:
+        """Returns the sensor working output range."""
+        return NotImplemented()
+
+    def read(self, normalize: bool = False, **_) -> float:
+        """Returns the sensor reading."""
+        value = self.robot.getDevice(self).getValue()
+        return adapt(value, self.range(), (0, 1)) if normalize else value
+
+    def enable(self, update_frequency: int):
+        """Enable the sensors to perceive information at the given frequency."""
+        self.robot.getDevice(self).enable(update_frequency)
+
+    def exists(self) -> bool:
+        """Check if the given device is available on the given robot."""
+        return not self.robot.getDevice(self) is None
