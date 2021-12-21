af70742767c42ead6edf11fc46ad3d20443bb8e4
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-21 14:20:39 +0100

    simplified epoch logic

--- controllers/runner/optimization/Epoch.py
+++ controllers/runner/optimization/Epoch.py
@@ -9,22 +9,9 @@ from typing import List, Dict, Callable
 class Epoch:
     """Evaluate the quality of connections to a network"""
 
-    def __init__(
-            self,
-            robot: EPuck,
-            sensors: List[int],
-            actuators: List[int],
-            evaluator: Callable[[EPuck], Fitness]
-    ):
-        # save data used during epoch run (it works as a log of states)
-        self.robot, self.sensors, self.actuators = robot, sensors, actuators
-
-        # save the controller to avoid changes in the robot reference
-        self.controller = robot.conductor
-
-        # set sensors to the controller
-        self.controller.sensors = dict(zip(self.robot.ir_sensors, sensors))
-        self.controller.actuators = dict(zip(self.robot.motors, actuators))
+    def __init__(self, robot: EPuck, evaluator: Callable[[EPuck], Fitness]):
+        # save robot to execute and controller for reference
+        self.robot, self.controller = robot, robot.conductor
 
         # define evaluation utils
         self.evaluator = evaluator(robot)
@@ -38,21 +25,17 @@ class Epoch:
 
     def step(self):
         """Run a simulation step and exec the required evaluations"""
-
-        self.robot.run(False)  # todo make configurable (e.g., raw_signals)
+        self.robot.run()
 
         # update fitness value
         self.evaluator.update()
 
         # save sensor readings and motors speeds for future analysis
-        self.stimulus += [{s: s.read(False) for s in self.robot.ir_sensors}]
+        self.stimulus += [{s: s.read() for s in self.robot.sensors}]
         self.response += [{m: m.speed for m in self.robot.motors}]
 
 
-def new_epoch(
-        robot: EPuck,
-        builder: Callable[[EPuck, List[int], List[int]], Epoch]
-) -> Epoch:
+def new_epoch(robot: EPuck, builder: Callable[[EPuck], Epoch]) -> Epoch:
     """Instantiate a new evaluation run for a robot"""
 
     # get the controller from the robot
@@ -60,47 +43,39 @@ def new_epoch(
 
     # select actuator nodes from the available nodes
     actuators = random_nodes(graph, set(), len(robot.motors))
+    robot.conductor.actuators = dict(zip(robot.motors, actuators))
 
     # select source nodes from the ones that are at least 2 steps far from the
     # outputs nodes
-    neighbor = minimum_distance_selection(
-        outputs=actuators,
-        distance=2,
-        negate=True
-    )(graph, list(), -1)
-    sensors = [*random_nodes(graph, neighbor, len(robot.ir_sensors))]
-
-    # return the new Epoch
-    epoch = builder(robot, sensors, [*actuators])
+    neighbor = minimum_distance_selection(actuators, 2, True)(graph, list(), -1)
+    sensors = list(random_nodes(graph, neighbor, len(robot.sensors)))
+    robot.conductor.sensors = dict(zip(robot.sensors, sensors))
 
     # initialize the network with the given setting
     robot.conductor.initialize()
 
-    return epoch
+    return builder(robot)
 
 
-def evolve_epoch(
-        epoch: Epoch,
-        builder: Callable[[EPuck, List[int], List[int]], Epoch]
-) -> Epoch:
+def evolve_epoch(epoch: Epoch, builder: Callable[[EPuck], Epoch]) -> Epoch:
     """
     Instantiate a new evaluation run for a robot as an evolution (in
     connections) of a previous one.
     """
 
-    # obtain the new sensors
-    sensors = mutate(
-        graph=epoch.controller.network,
-        sources=[*epoch.controller.sensors.values()], ground=-1,
-        probability=0.3, minimum_mutants=1, maximum_mutants=4,
-        viable_node_selection=minimum_distance_selection(
-            outputs={*epoch.controller.actuators.values()},
-            distance=2
-        )
-    )
-
     # avoid overwrite of controller's fields
     epoch.robot.conductor = copy(epoch.controller)
 
+    # obtain the new sensors and set them in the conductor
+    epoch.robot.conductor.sensors = dict(zip(epoch.robot.sensors, mutate(
+        graph=epoch.controller.network,
+        sources=list(epoch.controller.sensors.values()), ground=-1,
+        probability=0.3, minimum_mutants=1, maximum_mutants=4,
+        viable_node_selection=minimum_distance_selection(
+            outputs=set(epoch.controller.actuators.values()),
+            distance=2
+        )
+    )))
+
     # return the evolved epoch
-    return builder(epoch.robot, sensors, epoch.actuators)
+    return builder(epoch.robot)
