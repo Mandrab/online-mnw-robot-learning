cad18ba33f402710c8ea1ee8f7801fca56064166
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-21 14:47:05 +0100

    corrected task-specific epoch according to changes

--- controllers/runner/optimization/task/avoidance/fitness.py
+++ controllers/runner/optimization/task/avoidance/fitness.py
@@ -2,6 +2,7 @@ from functools import reduce
 from math import sqrt
 from operator import sub
 from optimization.Fitness import Fitness
+from robot.component.Motor import Motor
 from utils import adapt
 
 
@@ -12,16 +13,13 @@ class CollisionAvoidance(Fitness):
     __counter: int = 0
 
     def update(self):
-        # get the most near measure and make it in range 0-1
-        value = max(sensor.read() for sensor in self.robot.ir_sensors)
-
-        min_value, max_value = self.robot.ir_sensors_range()
+        # get the highest (nearer) proximity measure and make it in range 0-1
+        max_proximity = max(s.read(normalize=True) for s in self.robot.sensors)
 
         # get motors velocities and make them in range 0-1
         speeds = [motor.speed for motor in self.robot.motors]
-        speeds = [adapt(value, (-6.14, 6.14), (0.0, 1.0)) for value in speeds]
+        speeds = [adapt(value, Motor.range()) for value in speeds]
 
-        max_proximity = adapt(value, (min_value, max_value), (0, 1))
         average_speed = sum(speeds) / 2.0
         directions = sqrt(abs(reduce(sub, speeds)))
 
