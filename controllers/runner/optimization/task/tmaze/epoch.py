37c9c85822935d3a9a298200af87731ae1ab7e4f
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-23 10:08:08 +0100

    corrected bugs in tmaze evaluations

--- controllers/runner/optimization/task/tmaze/epoch.py
+++ controllers/runner/optimization/task/tmaze/epoch.py
@@ -3,10 +3,14 @@ import random
 from optimization.Epoch import Epoch as Base
 from optimization.Epoch import new_epoch as new, evolve_epoch as evolve
 from optimization.task.tmaze.fitness import TMaze
+from optimization.task.tmaze.world import Colors
 from robot.epuck import EPuck
 
-INITIAL_POSITION = [0, 0, 0.4]
+INITIAL_POSITION = [0, 3e-5, 0.4]
 INITIAL_ROTATION = [0, -1, 0, 0]
+
+BLACK_START = Colors.BLACK, [0, 2e-5, 0.25], [0, -1, 0.25]
+WHITE_START = Colors.WHITE, [0, -1, 0.25], [0, 2e-5, 0.25]
 
 
 class Epoch(Base):
@@ -19,37 +23,47 @@ class Epoch(Base):
         Base.__init__(self, robot, TMaze)
 
         # get position and rotation fields of robot controller
-        node = self.robot.getFromDef('evolvable')
-        self.translation_field = node.getField('translation')
-        self.rotation_field = node.getField('rotation')
+        self.robot_node = self.robot.getFromDef('evolvable')
+        self.translation_field = self.robot_node.getField('translation')
+        self.rotation_field = self.robot_node.getField('rotation')
 
     def step(self):
-        # every 'duration' steps, reset robot position and run the task again
-        # with random selected side to reach (defined by starting floor color)
+        """
+        Run a step of the robot and collect its result/fitness in it.
+        Periodically run the task again with random selected side to reach
+        (defined by starting floor color)
+        """
+
         if not self.counter % self.duration:
-
-            # get floors transaction fields
-            light = self.robot.getFromDef('light_floor').getField('translation')
-            dark = self.robot.getFromDef('dark_floor').getField('translation')
-
-            # randomly swap initial floors positions (basically, hide one)
-            if random.randint(0, 1):
-                light_level, dark_level = light.getSFVec3f(), dark.getSFVec3f()
-                light.setSFVec3f(dark_level)
-                dark.setSFVec3f(light_level)
 
             # reset robot to start position
             self.translation_field.setSFVec3f(INITIAL_POSITION)
             self.rotation_field.setSFRotation(INITIAL_ROTATION)
 
-            # notify evaluator that a new run is starting
-            self.evaluator.reset()
+            # ensure that changes take place
+            self.robot_node.resetPhysics()
+            self.robot.step(self.robot.run_frequency.ms)
 
-        # increment counter
-        self.counter += 1
+            # get floors transaction fields
+            light = self.robot.getFromDef('light_floor').getField('translation')
+            dark = self.robot.getFromDef('dark_floor').getField('translation')
+
+            # randomly set a floor as starting one (basically, hide the other)
+            if random.randint(0, 1):
+                starting_color, black_position, white_position = WHITE_START
+            else:
+                starting_color, black_position, white_position = BLACK_START
+            light.setSFVec3f(white_position)
+            dark.setSFVec3f(black_position)
+
+            # (re)set initial color of evaluator utils
+            self.evaluator.initial_color = starting_color
 
         # call super step
         Base.step(self)
+
+        # increment counter
+        self.counter += 1
 
 
 def new_epoch(robot: EPuck) -> Epoch: return new(robot, Epoch)
