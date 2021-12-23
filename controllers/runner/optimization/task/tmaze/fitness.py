d17ec1c5caf7b7cc69005a2f4af7ede4a0880def
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-23 10:06:48 +0100

    added direction evaluation in tmaze fitness

--- controllers/runner/optimization/task/tmaze/fitness.py
+++ controllers/runner/optimization/task/tmaze/fitness.py
@@ -1,3 +1,4 @@
+from operator import sub
 from optimization.Fitness import Fitness
 from robot.component import grounds
 from .world import Colors
@@ -12,8 +13,6 @@ class TMaze(Fitness):
     fitness: float = 0.0
     initial_color = Colors.NONE
 
-    def reset(self): self.initial_color = Colors.NONE
-
     def update(self):
         floor_level = next(iter(grounds(self.robot.sensors))).read()
 
@@ -25,12 +24,17 @@ class TMaze(Fitness):
         else:
             floor_level = Colors.GRAY
 
-        # at the first step, save the initial ground color
-        if Colors.NONE == self.initial_color:
-            self.initial_color = floor_level
-
         # if the target has been reached (we consider 3 color), increase fitness
         if floor_level != self.initial_color and floor_level != Colors.GRAY:
             self.fitness += 1
+
+        # calculate robot direction as left < 0, right > 0
+        robot_direction = sub(*map(lambda _: _.speed, self.robot.motors))
+
+        # if robot is moving toward target, increase fitness
+        if self.initial_color == Colors.BLACK:
+            self.fitness += robot_direction
+        elif self.initial_color == Colors.WHITE:
+            self.fitness -= robot_direction
 
     def value(self) -> float: return self.fitness
