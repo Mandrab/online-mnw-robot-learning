f1849e87bf14cc8195f67ce7046d28d82ad73092
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-27 13:07:57 +0100

    added dynamic behaviour to area avoidance

--- controllers/runner/optimization/task/avoidance/area/epoch.py
+++ controllers/runner/optimization/task/avoidance/area/epoch.py
@@ -1,12 +1,17 @@
-from optimization.Epoch import Epoch
+from optimization.task.avoidance.Epoch import Epoch as Base
 from optimization.Epoch import new_epoch as new, evolve_epoch as evolve
-from optimization.task.avoidance.area.fitness import AreaAvoidance as Evaluator
+from optimization.task.avoidance.area.fitness import AreaAvoidance
 from robot.epuck import EPuck
 
 
+class Epoch(Base):
+    @staticmethod
+    def objects(): return [f'area_{index}' for index in range(10)]
+
+
 def new_epoch(robot: EPuck) -> Epoch:
-    return new(robot, lambda epuck: Epoch(epuck, Evaluator))
+    return new(robot, lambda epuck: Epoch(epuck, AreaAvoidance))
 
 
 def evolve_epoch(epoch: Epoch) -> Epoch:
-    return evolve(epoch, lambda epuck: Epoch(epuck, Evaluator))
+    return evolve(epoch, lambda epuck: Epoch(epuck, AreaAvoidance))
