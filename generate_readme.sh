121e6d9af8cd80985b803ea699668d1209a3cf93
Paolo Baldini <paolobaldini01@gmail.com>
2021-12-31 11:31:28 +0100

    cleaned generate readme code

--- generate_readme.sh
+++ generate_readme.sh
@@ -1,34 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 # Generate a partial template of the run simulation
 
 # configurations files
-CONFIGS=`cat controllers/runner/config/simulation.py`
-ROBOT=`cat controllers/runner/robot/epuck.py`
-UTILS=`cat controllers/runner/optimization/utils.py`
+CONFIGS=$(cat controllers/runner/config/simulation.py)
+ROBOT=$(cat controllers/runner/robot/epuck.py)
+UTILS=$(cat controllers/runner/optimization/utils.py)
 
 # get fields from configs
-DURATION=`cat <<<$CONFIGS | awk -F 'epoch_duration =' '$2{print $2}'`
-EPOCHS=`cat <<<$CONFIGS | awk -F 'epoch_count = ' '$2{print $2}'`
-FITNESS=`cat <<<$CONFIGS | awk -F 'MINIMUM_FITNESS = ' '$2{print $2}'`
-LOAD=`cat <<<$CONFIGS | awk -F 'robot.motors_load = ' '$2{print $2}'`
-REPLICA=`cat <<<$CONFIGS | awk -F 'replica_count = ' '$2{print $2}'`
-SEED=`echo $CONFIGS | grep -F 'random.seed' | awk -F '[()]' '$2{print$2}'`
-TASK=`cat <<<$CONFIGS | awk -F 'task = Tasks.' '$2{print $2}'`
+DURATION=$(cat <<<"$CONFIGS" | awk -F 'epoch_duration = ' '$2{print $2}')
+EPOCHS=$(cat <<<"$CONFIGS" | awk -F 'epoch_count = ' '$2{print $2}')
+FITNESS=$(cat <<<"$CONFIGS" | awk -F 'MINIMUM_FITNESS = ' '$2{print $2}')
+LOAD=$(cat <<<"$CONFIGS" | awk -F 'robot.motors_load = ' '$2{print $2}')
+REPLICA=$(cat <<<"$CONFIGS" | awk -F 'replica_count = ' '$2{print $2}')
+SEED=$(echo "$CONFIGS" | awk -F 'random.seed[(]' '$2{sub(")", ""); print $2}')
+TASK=$(cat <<<"$CONFIGS" | awk -F 'task = Tasks.' '$2{print $2}')
 
 # get other fields
-FREQUENCY=`cat <<<$ROBOT | grep -F 'Frequency(' | awk -F 'hz_value=|)' '{print $2}'`
-LENGTH=`cat <<<$UTILS | awk -F 'WIRES_LENGTH = ' '$2{print $2}'`
-SIZE=`cat <<<$UTILS | awk -F 'DEVICE_SIZE = ' '$2{print $2}'`
+FREQUENCY=$(cat <<<"$ROBOT" | awk -F 'hz_value=' '$2{sub(")", ""); print $2}')
+LENGTH=$(cat <<<"$UTILS" | awk -F 'WIRES_LENGTH = ' '$2{print $2}')
+SIZE=$(cat <<<"$UTILS" | awk -F 'DEVICE_SIZE = ' '$2{print $2}')
+GIT_HASH=$(git rev-parse HEAD)
 
 # data from log file
-START_DATE=`cat $1 | grep -F '[' | head -n 1 | awk -F '[|[[:space:]]' '{print $2}'`
-START_TIME=`cat $1 | grep -F '[' | head -n 1 | awk '{print $2}'`
-END_DATE=`cat $1 | grep -F '[' | tail -n 1 | awk -F '[|[[:space:]]' '{print $2}'`
-END_TIME=`cat $1 | grep -F '[' | tail -n 1 | awk '{print $2}'`
-WORLD_FILE=`cat $1 | grep 'Running simulation in' | awk -F ' ' '{print $4}'`
+START_DATE=$(grep -F '[' "$1" | head -n 1 | awk -F '[|[[:space:]]' '{print $2}')
+START_TIME=$(grep -F '[' "$1" | head -n 1 | awk '{print $2}')
+END_DATE=$(grep -F '[' "$1" | tail -n 1 | awk -F '[|[[:space:]]' '{print $2}')
+END_TIME=$(grep -F '[' "$1" | tail -n 1 | awk '{print $2}')
+WORLD_FILE=$(grep 'Running simulation in' "$1" | awk -F ' ' '{print $4}')
 
 # create readme file
-echo "
+echo "\
 EXECUTIONS RESULTS
 Date: [$START_DATE, $END_DATE]
 Start time: $START_TIME
@@ -49,14 +50,14 @@ Configuration:
       - epuck freq: $FREQUENCY Hz
       - sensors range:
       - motor range:
-      - actuators resistance: $LOAD
+      - actuators resistances: $LOAD
     4. network:
       - network range: [0.0, 10.0]
       - device size: $SIZE
       - mean wires length: $LENGTH
       - std length: 0.35 * $LENGTH
     5. others:
-    - git-head hash: `git rev-parse HEAD`
+      - git-head hash: $GIT_HASH
 --------------------------------------------------------------------------------
-`cat $1 | grep '^[fitness|Creation]'`
+$(grep '^[fitness|Creation]' < "$1")
 "
