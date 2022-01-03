ed78bc5befef6f6e4aa092e8d344ad74b29169be
Paolo Baldini <paolobaldini01@gmail.com>
2022-01-03 10:31:44 +0100

    improved fitness calculation of tmaze task and added comment

--- controllers/runner/optimization/task/tmaze/fitness.py
+++ controllers/runner/optimization/task/tmaze/fitness.py
@@ -1,19 +1,35 @@
 from optimization.Fitness import Fitness
 from robot.component import grounds
+from utils import adapt
 from world.Colors import Colors
 
 
 class TMaze(Fitness):
     """
     Calculate the fitness depending on the capability of reach the end of the
-    maze and staying there as long as possible.
+    maze and staying there as long as possible. At each step the evaluator
+    calculate fitness according to the following formula:
+        2 * ~(f(i) or f(GRAY)) - 1 * f(i)
+    where:
+        - i in [BLACK, WHITE]: is the initial color of the floor.
+        - f return [0, 1]: is a function that says if the robot is over the
+          given floor. The return is 1 if it is on it, 0 otherwise.
+        - ~: is a negate function.
+    The formula gives a prize if the robot is not in the initial or gray floor
+    and gives a penalty if it is in the initial one. The fitness are then
+    averaged and the resulting range [-1, 2] is adapted to the range [0, 100].
     """
 
-    fitness: float = 0.0
-    counter: float = 0.0
     initial_color = Colors.NONE
 
+    fitness: float = 0.0
+    counter: int = 0
+
     def update(self):
+        # increment iteration counter
+        self.counter += 1
+
+        # get first ground-sensor reading
         floor_level = next(iter(grounds(self.robot.sensors))).read()
 
         # map reading to discrete values
@@ -30,6 +46,5 @@ class TMaze(Fitness):
         else:
             self.fitness -= 1
 
-        self.counter += 1
-
-    def value(self) -> float: return (self.fitness / self.counter + 1) / 0.03
+    def value(self) -> float:
+        return adapt(self.fitness / self.counter, (-1, 2), (0, 100))
