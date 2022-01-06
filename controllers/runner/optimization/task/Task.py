6f30a36a21ca456a98db6bd554b6b48e56a1319e
Paolo Baldini <paolobaldini01@gmail.com>
2022-01-06 23:27:59 +0100

    added data-structure to contain task's utils and configurations

---
+++ controllers/runner/optimization/task/Task.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from optimization.Fitness import Fitness
+from optimization.individual import Individual
+from robot.body import EPuck
+from typing import Callable, Tuple
+
+
+@dataclass(frozen=True)
+class Task:
+    """Group the task configurations and utils."""
+
+    life_manager: Callable[[Individual, int], None]
+    evaluator: Callable[[EPuck], Fitness]
+    sensors: Tuple[str]
