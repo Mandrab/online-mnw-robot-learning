09fef40e8ae24681b40ebadb9c5ca5468ecccdc4
Paolo Baldini <paolobaldini01@gmail.com>
2022-01-06 22:04:50 +0100

    added datastructure relative only to the evolution process

---
+++ controllers/runner/optimization/biography.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from optimization.Fitness import Fitness
+from typing import List, Dict
+
+
+@dataclass(frozen=True)
+class Biography:
+    """
+    Summarize the life of an individual. It contains the fitness obtained and
+    the history of the agent.
+    """
+
+    evaluator: Fitness
+    stimulus: List[Dict[str, float]] = field(default_factory=list)
+    response: List[Dict[str, float]] = field(default_factory=list)
