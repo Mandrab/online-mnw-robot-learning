c1c7c89fd65a24e8f7eddede3ddae4debae73c05
Paolo Baldini <paolobaldini01@gmail.com>
2022-01-06 17:54:45 +0100

    moved epuck file and changed its logical use

---
+++ controllers/runner/robot/body.py
@@ -0,0 +1,30 @@
+from controller import Supervisor
+from robot.component import sensor, enable
+from robot.component.Motor import Motor
+from utils import Frequency
+from typing import Iterable
+
+
+class EPuck(Supervisor):
+    """
+    Represents the body of the robot in the simulation. It includes a reference
+    to the sensors and actuators and act as a mediator to improve code quality.
+    It also includes some additional information specific to webots like the
+    update frequency of the controller.
+    """
+
+    # update/working time for robot modules
+    run_frequency = Frequency(hz_value=10)
+
+    # names of actuators (motors) actually used and their connection load
+    motors = tuple([Motor(f'{side} wheel motor') for side in ['left', 'right']])
+
+    def __init__(self, sensors: Iterable[str]):
+        Supervisor.__init__(self)
+
+        # initialize (existing) sensors and keep 'successful' ones
+        self.sensors = tuple(filter(enable(self), map(sensor, sensors)))
+
+        # initialize motors
+        for motor in self.motors:
+            motor.robot = self
