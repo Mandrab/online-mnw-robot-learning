8cdc0336037b20ad4304b38a3b5c2c0295277925
Paolo Baldini <paolobaldini01@gmail.com>
2022-01-26 15:21:48 +0100

    set ground with working range of ir

--- controllers/runner/robot/component/ground.py
+++ controllers/runner/robot/component/ground.py
@@ -5,6 +5,4 @@ from typing import Tuple
 class GroundSensor(Sensor):
     """Represents a robot ground sensor"""
 
-    # todo: in area avoidance it has to be influential: 0-1e3; in t-maze it has
-    # to be less influential: 0, 1e4
-    def range(self, **_) -> Tuple[float, float]: return 0, 1e4
+    def range(self, **_) -> Tuple[float, float]: return 0, 4095
