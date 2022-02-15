0de4622c9f1458609eec02f3687c16d9a08f808a
Paolo Baldini <paolobaldini01@gmail.com>
2022-02-15 15:09:46 +0100

    reduced threshold for ca task

--- controllers/runner/optimization/task/avoidance/collision/__init__.py
+++ controllers/runner/optimization/task/avoidance/collision/__init__.py
@@ -5,4 +5,4 @@ from optimization.task.avoidance.collision.fitness import Fitness
 sensors = tuple([f'ps{_}' for _ in range(8)])
 live = supplier([f'box_{_}' for _ in range(20)], lambda _: min(max(_, -.5), .5))
 
-task_description = Task(live, Fitness, sensors, 50.0, 0.3, 0.1)
+task_description = Task(live, Fitness, sensors, 30.0, 0.3, 0.1)
