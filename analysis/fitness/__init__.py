0050580d2aad1fba3cde38ec44a8d65f5d0d5343
Paolo Baldini <paolobaldini01@gmail.com>
2022-02-23 14:11:49 +0100

    optimised plotting and added function

--- analysis/fitness/__init__.py
+++ analysis/fitness/__init__.py
@@ -2,6 +2,7 @@ import matplotlib.patches as ptc
 import matplotlib.pyplot as plt
 
 from collections import defaultdict
+from functools import reduce
 from itertools import product
 from typing import Dict, List, Any, Callable
 
@@ -20,38 +21,46 @@ def transform(data: Dict[Any, List[Any]], strategy: Callable):
     return {k: strategy(vs) for k, vs in data.items()}
 
 
-def boxplot(p_title: str, x_label: str, y_label: str, data: Dict[str, List]):
+def boxplot(p_title: str, y_label: str, x_label: str, data: Dict[str, List]):
     """Create a boxplot with x = dict keys; y = dict value."""
     fig, ax = plt.subplots()
 
-    ax.boxplot(data.values())
+    data = sorted(data.items(), key=lambda _: _[0][0], reverse=True)
+    ax.boxplot([v for _, v in data], vert=False)
 
     ax.set(title=p_title, xlabel=x_label, ylabel=y_label)
-    ax.set_xticks([*range(len(data) + 2)], labels=[None, *data, None])
+    labels = [None, *[k for k, _ in data], None]
+    ax.set_yticks([*range(len(data) + 2)], labels=labels)
     ax.yaxis.grid(True, linestyle='dotted')
 
     plt.tight_layout()
     plt.show()
 
 
-def statistics(data: List[Dict], by_key: str):
+def statistics(data: List[Dict], by_key: str, limits: List[float]):
     """
     Print some statistics related to fitness according to a specific property of
     the configuration.
     """
+    if len(limits := sorted(limits)) <= 0:
+        return
+
+    ranges = [(float('-inf'), next(iter(limits)))] + [*zip(limits, limits[1:])]
+    ranges += [(next(iter(reversed(limits))), float('inf'))]
+
     data = group(data, by_key)
     data = {k: [max(v['fitness']) for v in vs] for k, vs in data.items()}
 
     def to_table(*strings) -> str:
         return '\t\t\t| '.join(map(str, strings))
 
-    print(to_table(by_key, '_ < 40') + '\t\t| 40 <= _ < 50' + '\t| _ >= 50')
+    print(to_table(by_key, '\t| '.join(['[%.1f, %.1f)' % _ for _ in ranges])))
 
     def percentage(key_range) -> float:
         filtered = list(filter(key_range[1], data[key_range[0]]))
         return 100.0 * len(list(filtered)) / len(data[key_range[0]])
 
-    ranges = [lambda _: _ < 40, lambda _: 40 <= _ < 50, lambda _: _ >= 50]
+    ranges = [lambda _: a <= _ < b for a, b in ranges]
 
     total = 0.0, 0.0, 0.0
     for by_key in data:
@@ -79,10 +88,23 @@ def evolutions(data: List[Dict], by_key: str):
 
         ax.plot(averages, color=colors(key_i), label=k)
 
-    plt.title('Average fitness evolution according to iterations')
+    ax.set(xlabel='Iterations', ylabel='Fitness')
+
+    plt.title('Fitness evolution according to iterations (average of maxima)')
     patches = [
         ptc.Patch(color=colors(i), label=by_key + ' ' + formats(k))
         for i, k in enumerate(groups)
     ]
     plt.legend(handles=patches, loc='lower right')
     plt.show()
+
+
+def evolution(data: Dict) -> Dict:
+    return reduce(
+        lambda a, b: a | dict(b),
+        [
+            ((f'initial {k}', [_[0] for _ in v]), (f'max {k}', [*map(max, v)]))
+            for k, v in dict(data).items()
+        ],
+        defaultdict(list)
+    )
