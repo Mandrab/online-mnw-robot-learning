31156b787a69f805e5117f75211f1629d0591f5c
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-28 10:29:16 +0100

    added generic transducer type

---
+++ controllers/runner/robot/transducer/transducer.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from controller import Robot
+from typing import Generic, Tuple, TypeVar
+from utils import adapt
+
+T = TypeVar("T")
+
+
+class Transducer(ABC, Generic[T]):
+    """
+    Represents a component of the robot that can be set or read.
+    In case of readable (i.e., a sensor), the value may belong
+    to a given range.
+    """
+
+    # represents the parent robot of the transducer
+    _robot: Robot
+
+    def initialize(self, robot: Robot):
+        """Initialize the transducer setting the parent robot."""
+
+        self._robot = robot
+
+    @abstractmethod
+    def range(self, reverse: bool = False) -> Tuple[T, T]:
+        """Returns the transducer working range."""
+        return NotImplemented()
+
+    @property
+    @abstractmethod
+    def value(self) -> T:
+        """Returns the transducer state."""
+        return NotImplemented()
+
+    @property
+    def normalized_value(self):
+        """Return the transducer state in range [0-1]."""
+        return adapt(self.value, self.range())
+
+    @value.setter
+    @abstractmethod
+    def value(self, v: T):
+        """Set the transducer state."""
+        NotImplemented()
