6dfbebe3c99b3999815cd42d09f5458f653a8926
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-28 10:34:08 +0100

    made the robot (supervisor) an updatable singleton

--- controllers/runner/robot/body.py
+++ controllers/runner/robot/body.py
@@ -1,6 +1,6 @@
 from controller import Supervisor
-from robot.component import sensor, enable
-from robot.component.motor import Motor
+from robot.transducer.motor import Motor
+from robot.transducer.transducer import Transducer
 from utils import Frequency
 from typing import Iterable
 
@@ -16,15 +16,40 @@ class EPuck(Supervisor):
     # update/working time for robot modules
     run_frequency = Frequency(hz_value=10)
 
-    # names of actuators (motors) actually used and their connection load
-    motors = tuple([Motor(f'{side} wheel motor') for side in ['left', 'right']])
+    __create_key = object()     # allows the constructor to be called only internally
+    _instances = {}             # used to create a singleton class
 
-    def __init__(self, sensors: Iterable[str]):
-        Supervisor.__init__(self)
+    @classmethod
+    def including(
+            cls,
+            sensors: Iterable[Transducer],
+            motors: Iterable[Transducer] = tuple([Motor(f'{side} wheel motor') for side in ['left', 'right']])
+    ):
+        """
+        Returns an EPuck instance with the given transducers.
+        It should be the default way to get an EPuck instance.
+        If it is called for the first time, create a new EPuck instance.
+        If an EPuck instance already exists, set the new transducers and return it.
+        """
 
-        # initialize (existing) sensors and keep 'successful' ones
-        self.sensors = tuple(filter(enable(self), map(sensor, sensors)))
+        # create or update the instance
+        if cls not in cls._instances:
+            cls._instances[cls] = EPuck(cls.__create_key)
+        else:
+            cls._instances[cls].sensors = sensors
+            cls._instances[cls].motors = motors
+
+        # initialize sensors
+        for sensor in sensors:
+            sensor.initialize(cls._instances[cls])
 
         # initialize motors
-        for motor in self.motors:
-            motor.robot = self
+        for motor in motors:
+            motor.initialize(cls._instances[cls])
+
+        return cls._instances[cls]
+
+    def __init__(self, create_key):
+        """Only internally callable constructor."""
+        assert create_key == EPuck.__create_key, "Private constructor. Use `new` method to get an instance."
+        Supervisor.__init__(self)
