e3b66961070b5fa92f1b4d5eeb857a1b09fdf48f
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-28 10:53:51 +0100

    modified task class to be sound with the changes

--- controllers/runner/optimization/task/task.py
+++ controllers/runner/optimization/task/task.py
@@ -2,16 +2,16 @@ from dataclasses import dataclass
 from optimization.fitness import Fitness
 from optimization.individual import Individual
 from robot.body import EPuck
-from typing import Callable, Tuple
+from typing import Callable
 
 
 @dataclass(frozen=True)
 class Task:
     """Group the task configurations and utils."""
 
+    executor: Callable[[], EPuck]
     life_manager: Callable[[Individual, int], None]
     evaluator: Callable[[EPuck], Fitness]
-    sensors: Tuple[str]
     evolution_threshold: float
     creation_sigma: float
     mutation_sigma: float
