6a9e62c781a6b8f3dac77927bc2526cfcf1d6ab1
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-29 18:45:42 +0100

    added simulation file for foraging task

---
+++ controllers/runner/optimization/task/foraging/simulations.py
@@ -0,0 +1,25 @@
+from logger import logger
+from optimization.individual import Individual
+from robot.robot import run
+
+
+def live(instance: Individual, duration: int):
+    """
+    Evaluate the individual in the foraging task.
+    Runs the robot and collect the resulting score of the step.
+    """
+
+    # iterate for the epoch duration
+    for counter in range(duration):
+
+        # run the individual
+        stimulus, response = run(instance)
+
+        # save the biography for the step
+        instance.biography.evaluator.update()
+        instance.biography.stimulus.append(stimulus)
+        instance.biography.response.append(response)
+
+        logger.cortex_plot(instance)
+
+    logger.info('fitness: ' + str(instance.biography.evaluator.value()))
