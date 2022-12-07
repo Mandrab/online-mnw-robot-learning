1df8aa8616a4c2e707595fdec36b1223663bc5ca
Paolo Baldini <paolobaldini01@gmail.com>
2022-12-07 15:20:27 +0100

    added an additional ground sensor to the robot

--- controllers/runner/optimization/task/foraging/__init__.py
+++ controllers/runner/optimization/task/foraging/__init__.py
@@ -3,6 +3,7 @@ from optimization.task.task import Task
 from optimization.task.foraging.fitness import Fitness
 from optimization.task.foraging.simulations import live
 from robot.body import EPuck
+from robot.robot import Robot
 from robot.transducer import GroundSensor, IRSensor
 from robot.transducer.motor import Motor
 from robot.transducer.transducer import Transducer
@@ -119,7 +120,19 @@ class Gripper(str, Transducer[int]):
         self._close = not self._close
 
 
-sensors = [GroundSensor('gs0'), PreySensor('prey-sensor')] + [IRSensor(f'ps{_}') for _ in range(8)]
+class ReverseGroundSensor(GroundSensor):
+    """Ground sensor that returns 4095 if the floor is black and 0 if it is white."""
+
+    @property
+    def value(self) -> float: return 4095 - self._robot.getDevice(self).getValue()
+
+
+sensors = [
+    GroundSensor('gs0'), ReverseGroundSensor('gs2'),
+    PreySensor('prey-sensor')
+] + [
+    IRSensor(f'ps{_}') for _ in range(8)
+]
 motors = [Gripper('gripper')] + [Motor(f'{side} wheel motor') for side in ['left', 'right']]
 
 task_description = Task(lambda: EPuck.including(sensors, motors), live, Fitness, 70.0, 5.0, 2.5)
