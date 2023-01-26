7c3511d42afab188008e694545dcf1941ddb94a5
Paolo Baldini <paolobaldini01@gmail.com>
2023-01-26 11:08:42 +0100

    added utility function to find if the robot is facing an object

---
+++ controllers/runner/optimization/task/foraging/util.py
@@ -0,0 +1,26 @@
+from math import atan2, cos, isclose, sin
+
+
+def is_facing(robot, obj) -> bool:
+    """ Check if the robot is facing (i.e., is in front) the object. """
+
+    if not obj.getContactPoints():
+        return False
+
+    # get the robot position and direction
+    robot_x, _, robot_y = robot.getFromDef('evolvable').getPosition()
+    _, a, _, b = robot.getFromDef('evolvable').getField('rotation').getSFRotation()
+
+    # get obj contact point and calculate its angle/direction referred to the robot center
+    contact_x, _, contact_y = obj.getContactPoint(0)
+    contact_angle = atan2(robot_x - contact_x, robot_y - contact_y)
+
+    # calculate the cos and sin of the robot direction and collision point
+    cr, sr = cos(a * b), sin(a * b)
+    cc, sc = cos(contact_angle), sin(contact_angle)
+
+    # calculate if the robot faces an object on its north, east, sud, west
+    ns_facing = isclose(cc, cr, abs_tol=0.5) and sr * sc > 0
+    we_facing = isclose(sc, sr, abs_tol=0.5) and cr * cc > 0
+
+    return ns_facing or we_facing
