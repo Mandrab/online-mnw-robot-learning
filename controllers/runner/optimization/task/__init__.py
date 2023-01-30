ca2a28b7c3ed180b0ce18d0791f6695524ff0612
Paolo Baldini <paolobaldini01@gmail.com>
2023-01-30 11:10:35 +0100

    added new task to list

--- controllers/runner/optimization/task/__init__.py
+++ controllers/runner/optimization/task/__init__.py
@@ -2,6 +2,7 @@ from enum import Enum
 from .task import Task
 from .avoidance.area import task_description as area_avoidance
 from .avoidance.collision import task_description as collision_avoidance
+from .connected_coverage import task_description as connected_coverage
 from .run import task_description as run
 from .foraging import task_description as foraging
 from .tmaze import task_description as t_maze
@@ -17,6 +18,7 @@ class Tasks(Enum):
 
     AREA_AVOIDANCE = area_avoidance
     COLLISION_AVOIDANCE = collision_avoidance
+    CONNECTED_COVERAGE = connected_coverage
     RUN = run
     FORAGING = foraging
     T_MAZE = t_maze
