349be9afa17a9c725a5d74e8880b606c59a876e9
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-08 12:41:06 +0100

    updated readme 2 json converter

--- tools/readme2json.py
+++ tools/readme2json.py
@@ -2,79 +2,37 @@
 Converts the readme file of a run to a json dataset for an easier analysis.
 """
 
-import itertools
 import json
 import re
 import sys
 
-_, input_filename, output_filename, *_ = [*sys.argv, 'README.md', 'dataset.json']
+_, input_filename, output_filename, *_ = *sys.argv, 'README.md', 'dataset.json'
 
 print(f'Converting {input_filename} to json')
 
 json_objects = list()
 
 expression = 'Device density: ([0-9|\.]*), '
-expression += 'CC density: ([0-9|\.]*), '
+expression += 'CC #wires: ([0-9|\.]*), '
+expression += 'CC #junctions: ([0-9|\.]*), '
 expression += 'Motor load: ([0-9|e\+]*), '
 expression += 'Average sensor signal multiplication: ([0-9|\.]*)%'
 
 head_pattern = re.compile(expression)
-head_pattern_0 = re.compile('.* density: (.*), .* density: ([0-9|.]*)(.*)')
-head_pattern_1 = re.compile('Effective network density: (.*) wires.*')
 body_pattern = re.compile('fitness: (.*)')
-densities_pattern = re.compile(' *densities: (.*)')
-load_pattern = re.compile(' *actuators resistance: (.*)')
-replicas_pattern = re.compile(' *replicas: (.*)')
 
 with open(input_filename) as infile:
     for line in infile.readlines():
-        # get densities from line
-        if results := densities_pattern.search(line):
-            densities = map(float, json.loads(results.group(1)))
-            densities = itertools.product(densities, range(replicas))
-            densities = map(lambda _: _[0], densities)
 
-        # get load from line
-        if results := load_pattern.search(line):
-            load = results.group(1)
-
-        # get replicas from line
-        if results := replicas_pattern.search(line):
-            replicas = int(results.group(1))
-
-        # standard (new) format of readme files
+        # standard format of readme files
         if results := head_pattern.search(line):
-            density, cc_density, load, avg_multiplier = results.groups()
+            density, cc_wires, cc_junctions, load, avg_multiplier, *_ = results.groups()
             configuration = dict(
                 density=float(density),
-                cc_density=float(cc_density),
+                cc_wires=int(cc_wires),
+                cc_junctions=int(cc_junctions),
                 load=float(load),
                 avg_multiplier=float(avg_multiplier),
-                fitness=list()
-            )
-            json_objects.append(configuration)
-
-        # for old version of the readme files
-        elif results := head_pattern_0.search(line):
-            density, cc_density, *results = results.groups()
-            if results:
-                load = re.search('Motors load: (.*)', results[0]).group(1)
-            configuration = dict(
-                density=float(density),
-                cc_density=float(cc_density),
-                load=float(load),
-                avg_multiplier=1.0,
-                fitness=list()
-            )
-            json_objects.append(configuration)
-
-        # for older version of the readme files
-        elif results := head_pattern_1.search(line):
-            configuration = dict(
-                density=float(next(densities)),
-                cc_density=float(results.group(1)),
-                load=float(load),
-                avg_multiplier=1.0,
                 fitness=list()
             )
             json_objects.append(configuration)
@@ -82,7 +40,6 @@ with open(input_filename) as infile:
         # iteration fitness line
         if results := body_pattern.search(line):
             configuration['fitness'].append(float(results.group(1)))
-
 
 with open(output_filename, mode='w') as file:
     json.dump(json_objects, file, indent=4)
