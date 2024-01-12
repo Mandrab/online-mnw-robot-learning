c5ea4d9005814ca1bad21a357336b4181dc708a8
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-12 15:03:45 +0100

    added '''singleton''' supervisor

---
+++ controllers/runner2/supervisor.py
@@ -0,0 +1,6 @@
+from controller import Supervisor
+
+# create a webots supervisor, that is a robot AND a proxy to modify the environment programmatically
+supervisor = Supervisor()
+
+__all__ = "supervisor",
