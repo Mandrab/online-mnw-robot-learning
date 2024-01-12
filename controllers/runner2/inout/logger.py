a3b68c1d38d6ecec6fd87b241f68ed1d47d0328d
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-12 15:06:08 +0100

    added logger script

---
+++ controllers/runner2/inout/logger.py
@@ -0,0 +1,32 @@
+import logging
+import os
+import sys
+
+from inout.loader import configs
+
+LOGGER_NAME: str = "online-mnw-robot-learning"
+LOG_FORMAT: str = "[%(asctime)s %(levelname)s]\t %(message)s"
+LOG_PATH: str = configs["output_directory"]
+LOG_FILE: str = configs["log_file"]
+
+# obtain the logger instance
+logger = logging.getLogger(LOGGER_NAME)
+
+# calculate the log file position
+file_path = os.path.join(LOG_PATH, LOG_FILE)
+
+# define the file handler
+handler = logging.FileHandler(file_path)
+handler.setFormatter(logging.Formatter(LOG_FORMAT))
+logger.addHandler(handler)
+
+# define the shell handler
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter(LOG_FORMAT))
+logger.addHandler(handler)
+
+# display all the logs
+logger.setLevel(logging.DEBUG)
+
+# export only the logger instance
+__all__ = "logger",
