cc1f8fcea2672be3dc801f46d19bf18a0ff74f76
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-12 17:14:27 +0100

    made two classes frozen

--- controllers/runner2/control/controller.py
+++ controllers/runner2/control/controller.py
@@ -1,6 +1,7 @@
 import random
 
 from ctypes import byref, c_int
+from dataclasses import dataclass
 from inout.loader import configs
 from nnspy import connected_component, datasheet, network_state, network_topology, nns
 
@@ -9,6 +10,7 @@ PACKAGE_SIZE: int = configs["package_size"]
 WIRES_LENGTH: float = configs["wires_length"]
 
 
+@dataclass(frozen=True)
 class Controller:
 
     ds: datasheet
@@ -16,26 +18,29 @@ class Controller:
     ns: network_state
     cc: connected_component
 
-    def __init__(self, seed: int):
-
-        # create the datasheet of a network with the desired density
-        self.ds = datasheet(
-            wires_count=int(NETWORK_DENSITY * PACKAGE_SIZE ** 2 / WIRES_LENGTH ** 2),
-            length_mean=WIRES_LENGTH,
-            length_std_dev=WIRES_LENGTH * 0.35,
-            package_size=PACKAGE_SIZE,
-            generation_seed=seed,
-        )
-
-        # set the random seed
-        random.seed(seed)
-
-        # create the network topology, state and components
-        cc_count, n2c = c_int(0), (c_int * self.ds.wires_count)()
-        self.nt = nns.create_network(self.ds, n2c, byref(cc_count))
-        self.ns = nns.construe_circuit(self.ds, self.nt)
-        ccs = nns.split_components(self.ds, self.nt, n2c, cc_count)[:cc_count.value]
-        self.cc = max(ccs, key=lambda x: int(x.ws_count))
-
     def __str__(self):
         return f"Wires count: {self.ds.wires_count}, Junctions count: {self.nt.js_count}"
+
+
+def controller(seed: int):
+
+    # create the datasheet of a network with the desired density
+    ds = datasheet(
+        wires_count=int(NETWORK_DENSITY * PACKAGE_SIZE ** 2 / WIRES_LENGTH ** 2),
+        length_mean=WIRES_LENGTH,
+        length_std_dev=WIRES_LENGTH * 0.35,
+        package_size=PACKAGE_SIZE,
+        generation_seed=seed,
+    )
+
+    # set the random seed
+    random.seed(seed)
+
+    # create the network topology, state and components
+    cc_count, n2c = c_int(0), (c_int * ds.wires_count)()
+    nt = nns.create_network(ds, n2c, byref(cc_count))
+    ns = nns.construe_circuit(ds, nt)
+    ccs = nns.split_components(ds, nt, n2c, cc_count)[:cc_count.value]
+    cc = max(ccs, key=lambda x: int(x.ws_count))
+
+    return Controller(ds, nt, ns, cc)
--- controllers/runner2/control/history.py
+++ controllers/runner2/control/history.py
@@ -1,23 +1,19 @@
 from control.controller import Controller
 from control.interface import Interface
 from control.tsetlin.state import State
-from nnspy import nns
+from dataclasses import dataclass
 from typing import List
 
 
+@dataclass(frozen=True)
 class History:
 
-    # arrays where to memorize the performance and state values, the control, and the interface information
+    # arrays where to memorize the control, the interface, the performance and state values
+    initial_controller: Controller
+    controller_index: int
     performance_collection: List[float]
     state_collection: List[State.Type]
     interface_collection: List[Interface]
-
-    def __init__(self, controller: Controller, index: int):
-        self.initial_controller = controller
-        self.controller_index = index
-        self.performance_collection = []
-        self.state_collection = []
-        self.interface_collection = []
 
     def save_performance(self, performance: float):
         self.performance_collection.append(performance)
@@ -27,3 +23,7 @@ class History:
 
     def save_interface(self, interface: Interface):
         self.interface_collection.append(interface.copy())
+
+
+def history(controller: Controller, index: int):
+    return History(controller, index, [], [], [])
