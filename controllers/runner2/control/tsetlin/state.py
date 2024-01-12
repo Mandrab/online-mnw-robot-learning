6fb6c0314b2d6408e0a69c4a4f8d584240596cc2
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-12 15:15:33 +0100

    added Tsetlin state class

---
+++ controllers/runner2/control/tsetlin/state.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from enum import Enum
+from typing import Dict
+
+
+class State:
+
+    class Type(Enum):
+        OPERATION = 0
+        ADAPTATION = 1
+        EXPLORATION = 2
+
+    class Transition(Enum):
+        PERFORMANCE_INCREASE = 0
+        PERFORMANCE_STAGNATION = 1
+        PERFORMANCE_DECREASE = 2
+
+    type: Type
+    transition: Dict[Transition, int]
+
+    def __init__(self, state_type: Type = Type.OPERATION):
+        self.type = state_type
+        self.transition = {}
+
+
+def str2phase(s: str) -> State.Type:
+    if s == "exploration":
+        return State.Type.EXPLORATION
+    elif s == "adaptation":
+        return State.Type.ADAPTATION
+    return State.Type.OPERATION
