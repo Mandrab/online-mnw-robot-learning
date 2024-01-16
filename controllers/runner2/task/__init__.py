993322a80e049afc5078f8047509961c043e12ef
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-16 10:35:22 +0100

    corrected config loading and cleaned code duplicate

--- controllers/runner2/task/__init__.py
+++ controllers/runner2/task/__init__.py
@@ -1,7 +1,7 @@
 from inout.loader import configs
 from task.collision_avoidance.evaluator import evaluate as collision_avoidance_evaluation
 
-TASK_TYPE = configs["task_type"]
+TASK_TYPE = configs["task"]["type"]
 
 
 if TASK_TYPE == "COLLISION_AVOIDANCE":
