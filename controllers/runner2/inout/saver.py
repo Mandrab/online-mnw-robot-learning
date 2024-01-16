14342122999c3bdb9883b296bfc32c3dc1b72fe4
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-16 11:10:20 +0100

    corrected file saver

--- controllers/runner2/inout/saver.py
+++ controllers/runner2/inout/saver.py
@@ -1,13 +1,14 @@
 import os
 
-from control.history import History
 from inout.loader import configs
 from nnspy import nns
+from simulation.replica import Replica
+from typing import Tuple
 
-OUTPUT_DIRECTORY: str = configs["output_directory"]
-PERFORMANCE_FILE: str = configs["performance_file"]
-STATE_FILE: str = configs["state_file"]
-COUPLING_FILE: str = configs["coupling_file"]
+OUTPUT_DIRECTORY: str = configs["output"]["path"]
+PERFORMANCE_FILE: str = configs["output"]["performance_file"]
+STATE_FILE: str = configs["output"]["state_file"]
+COUPLING_FILE: str = configs["output"]["coupling_file"]
 
 # calculate the performance and state files position
 performance_path: str = os.path.join(OUTPUT_DIRECTORY, PERFORMANCE_FILE)
@@ -15,33 +16,31 @@ state_path: str = os.path.join(OUTPUT_DIRECTORY, STATE_FILE)
 couplings_path: str = os.path.join(OUTPUT_DIRECTORY, COUPLING_FILE)
 
 
-def save(data: History):
+def save(enumerated_replica: Tuple[int, Replica]):
+
+    index, replica = enumerated_replica
 
     # write the recorded performance to file
     with open(performance_path, "a") as file:
-        file.write(", ".join(map(str, data.performance_collection)) + "\n")
+        performances = [conf.configuration.performance for conf in replica.history]
+        file.write(", ".join(map(str, performances)) + "\n")
 
     # write the recorded states to file
     with open(state_path, "a") as file:
-        file.write(", ".join(map(str, data.state_collection)) + "\n")
+        states = [conf.tsetlin_index for conf in replica.history]
+        file.write(", ".join(map(str, states)) + "\n")
 
     # save the interface used in each simulation epoch
-    for idx, it in enumerate(data.interface_collection):
-        nns.serialize_interface(it, OUTPUT_DIRECTORY.encode('utf-8'), data.controller_index, idx)
+    for idx, it in enumerate([conf.configuration.interface for conf in replica.history]):
+        nns.serialize_interface(it.c_interface, OUTPUT_DIRECTORY.encode('utf-8'), index, idx)
 
-        # join the pin and multiplier information for sensory couplings
-        couplings = {n: (it.mapping[n], v) for n, v in it.multipliers.items()}
-
-        # create a dictionary with all the information (pin and multiplier for the sensors, pin for the motors) joined
-        couplings = it.mapping | couplings
-
-        # save the dictionary to file
+        # save the dictionary with the pin and load/multiplier to file
         with open(couplings_path, "a") as file:
-            file.write(couplings.__str__() + "\n")
+            file.write(it.items.__str__() + "\n")
 
     # save the control information
-    ds, nt = data.initial_controller.ds, data.initial_controller.nt
-    ns, cc = data.initial_controller.ns, data.initial_controller.cc
-    nns.serialize_network(ds, nt, OUTPUT_DIRECTORY.encode('utf-8'), data.controller_index)
-    nns.serialize_state(ds, nt, ns, OUTPUT_DIRECTORY.encode('utf-8'), data.controller_index, 0)
-    nns.serialize_component(cc, OUTPUT_DIRECTORY.encode('utf-8'), data.controller_index, 0)
+    ds, nt = replica.network.ds, replica.network.nt
+    ns, cc = replica.network.ns, replica.network.cc
+    nns.serialize_network(ds, nt, OUTPUT_DIRECTORY.encode('utf-8'), index)
+    nns.serialize_state(ds, nt, ns, OUTPUT_DIRECTORY.encode('utf-8'), index, 0)
+    nns.serialize_component(cc, OUTPUT_DIRECTORY.encode('utf-8'), index, 0)
