1a4a9b0cea3700eef4f3ead2a719b396e607e712
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-16 10:02:28 +0100

    created epoch function

---
+++ controllers/runner2/simulation/epoch.py
@@ -0,0 +1,48 @@
+from control.adapt import adapt
+from control.tsetlin.state import State
+from functools import reduce
+from inout.loader import configs
+from inout.logger import logger
+from simulation.history import Adaptation
+from simulation.replica import Replica
+from simulation.step import step
+from webots.supervisor import supervisor
+
+CONTINUOUS_EXPERIMENT: bool = configs["continuous_experiment"]
+EPOCHS_DURATION: int = configs["epochs_duration"]
+HISTORY_WEIGHT: float = configs["history_weight"]
+
+
+def run_epoch(replica: Replica, _: int) -> Replica:
+
+    # restore simulation to starting point if the task is not continuous
+    if not CONTINUOUS_EXPERIMENT:
+        supervisor.simulationReset()
+
+    # run the replica for the specified number of steps and collect the performance
+    replica = reduce(step, range(EPOCHS_DURATION), replica)
+
+    # calculate the average performance per step
+    replica.configuration.performance /= EPOCHS_DURATION
+
+    # log the performance to console and to file
+    logger.info(f"performance: {replica.configuration.performance}")
+
+    # save the information relative to the current epoch
+    replica.history.append(Adaptation(replica.configuration, replica.tsetlin.state.type, replica.tsetlin.state_idx))
+
+    # if the best known configuration is in use, update its overall evaluation
+    if replica.tsetlin.state.type == State.Type.OPERATION:
+        replica.history.best_configuration.performance *= HISTORY_WEIGHT
+        replica.history.best_configuration.performance += (1 - HISTORY_WEIGHT) * replica.configuration.performance
+    # if a new configuration is in use and its performance is higher than the best known one, set it as the new best
+    elif replica.configuration.performance > replica.history.best_configuration.performance:
+        replica.history.best_configuration = replica.configuration
+
+    # adapt the control configuration of the replica
+    replica.configuration = adapt(replica)
+
+    return replica
+
+
+__all__ = "run_epoch",
