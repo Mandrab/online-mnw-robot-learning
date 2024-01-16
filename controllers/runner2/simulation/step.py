2e90cd9c7db3910ffc88e2e6e3e9ad72386b51b8
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-16 10:00:54 +0100

    simplified simulation step

--- controllers/runner2/simulation/step.py
+++ controllers/runner2/simulation/step.py
@@ -1,18 +1,14 @@
-from replica import Replica, run
+from simulation.replica import Replica, run
 from task import evaluate
-from typing import Tuple
 
 
-def step(replica_performance: Tuple[Replica, float], _: int) -> Tuple[Replica, float]:
-
-    # decompose the input data
-    replica, performance = replica_performance
+def step(replica: Replica, _: int) -> Replica:
 
     # run this replica in a simulation step
     run(replica)
 
     # evaluate the replica performance in this simulation step
-    performance += evaluate()
+    replica.configuration.performance += evaluate()
 
     # returns the replica with the updated performance evaluation
-    return replica, performance
+    return replica
