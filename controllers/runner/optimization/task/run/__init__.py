a940123d2bb067fc7586e4eefbfaa63eece5cd5c
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-18 14:02:13 +0100

    switched between the two versions of the controller

--- controllers/runner/optimization/task/run/__init__.py
+++
@@ -1,7 +0,0 @@
-from optimization.individual import live
-from optimization.task.task import Task
-from optimization.task.run.fitness import Fitness
-
-sensors = tuple([f'ps{_}' for _ in range(8)])
-
-task_description = Task(live, Fitness, sensors, 50.0, 0.3, 0.1)
