8efa8577c20b67c28a322cb7dd5e32ffeeaa6d12
Paolo Baldini <paolobaldini01@gmail.com>
2024-01-18 13:04:23 +0100

    added foraging tast to list of tasks

--- controllers/runner2/task/__init__.py
+++ controllers/runner2/task/__init__.py
@@ -1,11 +1,14 @@
 from inout.loader import configs
 from task.collision_avoidance.step import step as collision_avoidance_step
+from task.foraging.step import step as foraging_step
 from task.tmaze.step import step as tmaze_step
 
 TASK_TYPE = configs["task"]["type"]
 
 if TASK_TYPE == "COLLISION_AVOIDANCE":
     step = collision_avoidance_step
+if TASK_TYPE == "FORAGING":
+    step = foraging_step
 if TASK_TYPE == "T-MAZE":
     step = tmaze_step
 
