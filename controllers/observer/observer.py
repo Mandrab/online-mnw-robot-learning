3ced010594f9b85fb3ff6089a01d4668ce242cc5
Paolo Baldini <paolobaldini01@gmail.com>
2024-03-13 14:55:18 +0100

    now observer even saves recordings

--- controllers/observer/observer.py
+++ controllers/observer/observer.py
@@ -6,54 +6,67 @@ from itertools import combinations
 
 TIME_STEP = configs["task"]["time_step_ms"]
 ROBOTS_COUNT = configs["task"]["robots_count"]
+EPOCHS_COUNT = configs["task"]["epochs_count"]
+EPOCH_DURATION = configs["task"]["epochs_duration"]
 
 RANGE = 0.15
 TOUCH_RANGE = 0.08
 
 supervisor = Supervisor()
 
-while True:
+for epoch in range(EPOCHS_COUNT):
 
-    # if the webots simulation is paused or stopped, return
-    if supervisor.step(TIME_STEP) == -1:
-        pass
+    Css = list()
+    Sss = list()
 
-    # get the position of each robot
-    positions = np.array([
-        np.array(supervisor.getFromDef(f"Elisa-3_{i + 1}").getPosition())[[0, 2]]
-        for i in range(ROBOTS_COUNT)
-    ])
+    for step in range(EPOCH_DURATION):
 
-    distances = dict()
+        # if the webots simulation is paused or stopped, return
+        while supervisor.step(TIME_STEP) == -1:
+            pass
 
-    # calculate the distance between each robot
-    for a, b in combinations(range(ROBOTS_COUNT), 2):
-        distance = np.linalg.norm(positions[a] - positions[b])
-        distances[a, b] = distances[b, a] = distance
+        # get the position of each robot
+        positions = np.array([
+            np.array(supervisor.getFromDef(f"Elisa-3_{i + 1}").getPosition())[[0, 2]]
+            for i in range(ROBOTS_COUNT)
+        ])
 
-    Cs = np.empty(shape=(ROBOTS_COUNT, 1))
-    Ss = np.empty(shape=(ROBOTS_COUNT, 1))
+        distances = dict()
 
-    for a in range(ROBOTS_COUNT):
+        # calculate the distance between each robot
+        for a, b in combinations(range(ROBOTS_COUNT), 2):
+            distance = np.linalg.norm(positions[a] - positions[b])
+            distances[a, b] = distances[b, a] = distance
 
-        # calculate the cohesion factor of each robot: the percentage of robots it is near to
+        Cs = np.empty(shape=(ROBOTS_COUNT, 1))
+        Ss = np.empty(shape=(ROBOTS_COUNT, 1))
 
-        # find the neighbors of each robot
-        neighbors = [b for b in range(ROBOTS_COUNT) if a != b and distances[a, b] < RANGE]
+        for a in range(ROBOTS_COUNT):
 
-        Cs[a] = len(neighbors) / (ROBOTS_COUNT - 1)
+            # calculate the cohesion factor of each robot: the percentage of robots it is near to
 
-        if not neighbors:
-            Cs[a] = 0
-            Ss[a] = 0
-            continue
+            # find the neighbors of each robot
+            neighbors = [b for b in range(ROBOTS_COUNT) if a != b and distances[a, b] < RANGE]
 
-        # calculate the separation factor of each robot: the percentage of neighbors it touches
+            Cs[a] = len(neighbors) / (ROBOTS_COUNT - 1)
 
-        # find pseudo-contacts with other robots
-        contacts = [b for b in neighbors if distances[a, b] < TOUCH_RANGE]
+            if not neighbors:
+                Cs[a] = 0
+                Ss[a] = 0
+                continue
 
-        # the performance is inverse to the contacts: the less, the better
-        Ss[a] = len(contacts) / ROBOTS_COUNT
+            # calculate the separation factor of each robot: the percentage of neighbors it touches
 
-    print(Cs.mean(), Ss.mean())
+            # find pseudo-contacts with other robots
+            contacts = [b for b in neighbors if distances[a, b] < TOUCH_RANGE]
+
+            # the performance is inverse to the contacts: the less, the better
+            Ss[a] = len(contacts) / ROBOTS_COUNT
+
+        Css.append(Cs.mean())
+        Sss.append(Ss.mean())
+
+    with open("cohesion.dat", "a") as file:
+        file.write(", ".join(map(str, Css)) + "\n")
+    with open("separation.dat", "a") as file:
+        file.write(", ".join(map(str, Sss)) + "\n")
